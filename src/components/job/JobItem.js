96dd65d83acede890f1c921180939f35d83cd7d5
Nicolas Caro <trynx93@gmail.,com>
2022-06-05 19:50:37 +0300

    Add to jobs candidates and daysOpen data

--- src/components/job/JobItem.js
+++ src/components/job/JobItem.js
@@ -1,7 +1,17 @@
-import { useState } from "react";
+const daysDifference = (startDate, endDate) => {
+    const diffSeconds = endDate.getTime() - startDate.getTime();
+    const diffDays = Math.ceil(diffSeconds / (1000 * 3600 * 24));
+
+    return diffDays;
+};
 
 export default function JobItem({ jobData }) {
-    const { position, department, office, candidates, daysOpen } = jobData;
+    const { position, department, office, candidates, date_open } = jobData;
+
+    const openDay = new Date(date_open);
+    const currDay = new Date();
+
+    const daysOpen = daysDifference(openDay, currDay);
 
     const handleClick = (event) => {
         // TODO: Download resume
@@ -9,14 +19,13 @@ export default function JobItem({ jobData }) {
     };
 
     return (
-        <li className="card">
+        <li className='card'>
             <div
                 style={{
                     display: "flex",
                     alignItems: "baseline",
                     gap: "1rem",
-                }}
-            >
+                }}>
                 <h4> Position:</h4>
                 <p>{position}</p>
                 <h4> Department:</h4>
@@ -24,12 +33,12 @@ export default function JobItem({ jobData }) {
                 <h4> Office:</h4>
                 <p>{office}</p>
                 <h4> Candidates:</h4>
-                <p>{candidates}</p>
+                <p>{candidates.length}</p>
                 <h4> DaysOpen:</h4>
                 <p>{daysOpen}</p>
             </div>
-            <div className="actions">
-                <button className="btn" onClick={handleClick}>
+            <div className='actions'>
+                <button className='btn' onClick={handleClick}>
                     Push Me
                 </button>
             </div>
--- src/pages/JobsPage.js
+++ src/pages/JobsPage.js
@@ -1,5 +1,4 @@
 import { useCallback, useContext, useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
 import CreateJobButton from "../components/job/CreateJobButton";
 import JobList from "../components/job/JobList";
 import AuthContext from "../store/auth-context";
@@ -8,7 +7,6 @@ export default function AllJobs() {
     const [isLoading, setIsLoading] = useState(true);
     const [jobs, setJobs] = useState([]);
     const authCtx = useContext(AuthContext);
-    const history = useHistory();
 
     // TODO: Research how to do it globally and that don't need to manually add it for each request
     const retryToken = useCallback(async () => {
@@ -43,43 +41,51 @@ export default function AllJobs() {
         // TODO: Change as needed to the backend url
         const url = "http://localhost:8088/api/job/";
 
-        const result = await fetch(url, {
-            headers: {
-                "Content-type": "application/json",
-                "x-access-token": authCtx.token,
-            },
-        });
+        try {
+            const result = await fetch(url, {
+                headers: {
+                    "Content-type": "application/json",
+                    "x-access-token": authCtx.token,
+                },
+            });
 
-        const data = await result.json();
+            const data = await result.json();
 
-        if (!result.ok) {
-            if (!data) {
-                alert("Issue with retriving jobs");
+            if (!result.ok) {
+                if (!data) {
+                    alert("Issue with retriving jobs");
+                    return;
+                }
+
+                // Some error in the server while retriving the jobs
+                if (!data.isRetry) {
+                    alert(data.message);
+                    return;
+                }
+
+                const isTokenRefreshed = await retryToken();
+                if (!isTokenRefreshed) {
+                    // TODO: Can add better error showing
+                    setIsLoading(false);
+                    setJobs([]);
+                }
+
                 return;
             }
 
-            // Some error in the server while retriving the jobs
-            if (!data.isRetry) {
-                alert(data.message);
-                return;
-            }
+            const jobs = data;
 
-            const isTokenRefreshed = await retryToken();
-            if (!isTokenRefreshed) {
-                // TODO: Can add better error showing
-                setIsLoading(false);
-                setJobs([]);
-            }
-
-            return;
+            setIsLoading(false);
+            setJobs(jobs);
+        } catch (err) {
+            console.error("Error at JobPage fetch all jobs");
+            console.error(err);
         }
-
-        const jobs = data;
-
-        setIsLoading(false);
-        setJobs(jobs);
     }, [authCtx.token, retryToken]);
 
+    // FIXME: As for now there is a bug, when the token is refreshed
+    // when sent to create a new job, it'll refresh the token
+    // but all the jobData will be lost and not resend.
     const onCreateJob = useCallback(
         async (jobData) => {
             // TODO: Add axios instead of fetch?
@@ -138,7 +144,8 @@ export default function AllJobs() {
 
     return (
         <>
-            <JobList allJobs={jobs} />
+            {jobs.length === 0 && <p>There are not jobs yet, add a job</p>}
+            {jobs.length > 0 && <JobList allJobs={jobs} />}
             {/* TODO: Should be a floating button */}
             <CreateJobButton onCreateJob={onCreateJob} />
         </>
