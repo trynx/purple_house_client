d1e376898cec00b00a86a512be8c3ec511bf1b06
Nicolas Caro <trynx93@gmail.,com>
2022-06-08 03:12:19 +0300

    Make pretty Navigation bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import AuthPage from "./pages/AuthPage";
 import CandidatesPage from "./pages/CandidatesPage";
+import DashboardPage from "./pages/DashboardPage";
 import JobsPage from "./pages/JobsPage";
 import { useAuthCtx } from "./store/auth-context";
 import { JobContextProvider } from "./store/job-context";
@@ -25,6 +26,10 @@ function App() {
                     </Route>
                     <Route path='/candidates'>
                         {authCtx.isLoggedIn && <CandidatesPage />}
+                        {startPage}
+                    </Route>
+                    <Route path='/dashboard'>
+                        {authCtx.isLoggedIn && <DashboardPage />}
                         {startPage}
                     </Route>
                 </JobContextProvider>
--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,33 +1,39 @@
-import { Link } from "react-router-dom";
+import { Tabs } from "antd";
+import { useHistory } from "react-router-dom";
 import { useAuthCtx } from "../../store/auth-context";
+import RegularButton from "../../ui/button/RegularButton";
 import classes from "./MainNavigation.module.css";
+const { TabPane } = Tabs;
+
 export default function MainNavigation() {
     const authCtx = useAuthCtx();
+    const history = useHistory();
+
+    if (!authCtx.isLoggedIn) {
+        return <p>Please log in..</p>;
+    }
+
+    const changeHandler = (router) => {
+        history.replace(router);
+    };
 
     return (
-        <header className={classes.header}>
-            <Link to='/' className={classes.logo}>
-                Purple House
-            </Link>
-            <nav>
-                <ul>
-                    {authCtx.isLoggedIn && (
-                        <li>
-                            <Link to='/jobs'>Jobs</Link>
-                        </li>
-                    )}
-                    {authCtx.isLoggedIn && (
-                        <li>
-                            <Link to='/candidates'>Candidates</Link>
-                        </li>
-                    )}
-                    {authCtx.isLoggedIn && (
-                        <li>
-                            <button onClick={authCtx.logout}>Log Out</button>
-                        </li>
-                    )}
-                </ul>
-            </nav>
-        </header>
+        <Tabs
+            defaultActiveKey='1'
+            centered
+            className={classes}
+            tabBarStyle={{ color: "#6659e0" }}
+            style={{
+                marginTop: "2rem",
+                marginRight: "1rem",
+            }}
+            tabBarExtraContent={
+                <RegularButton onClick={authCtx.logout}>Log Out</RegularButton>
+            }
+            onChange={changeHandler}>
+            <TabPane tab='Candidates' key='/candidates' />
+            <TabPane tab='Jobs' key='jobs' />
+            <TabPane tab='Dashboard' key='/dashboard' />
+        </Tabs>
     );
 }
